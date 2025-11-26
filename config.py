39fcc40715b89b728e256aa31b0d2515f8f6b660
AustinMRivera <amrivera5@mail.fhsu.edu>
2025-11-26 13:59:22 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration file - keep sensitive info here (don't commit to Git in real projects)
+
+IPINFO_TOKEN = 'your_ipinfo_token_here'  # Get from ipinfo.io
+
+EMAIL_SENDER = 'your_email@gmail.com'
+EMAIL_PASSWORD = 'your_app_password'  # Use Gmail app password
+EMAIL_RECEIVER = 'receiver_email@example.com'
+
+DB_NAME = 'network.db'
